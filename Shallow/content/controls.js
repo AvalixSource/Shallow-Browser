855876ed023a026c74d00cf86f6959679466478c
Joshua Tulloch <sorioxcode@gmail.com>
2016-08-24 00:35:53 -0700

    Getting ready for file structure change

--- Shallow/content/controls.js
+++
@@ -1,3 +0,0 @@
-function closeApp() {
-  window.close();
-}
--- Shallow/main.js
+++
@@ -1,7 +0,0 @@
-const {app, BrowserWindow} = require('electron')
-
-app.on("ready", () => {
-  let win = new BrowserWindow({width:1200, height:800, 'minWidth':500, 'minHeight':400, frame:false})
-  win.loadURL('file://' + __dirname + '/index.html');
-  win.show();
-})
